4b20d9bfa48830342a22c089991f04cb37994f30
FrancescoLoCasto <francesco_locasto@libero.it>
2022-11-21 14:57:03 +0100

    insert euro entity

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const kilomNumbers = parseFloat(prompt("quanta distanza devi percorrere?"))
+const kilomNumbers = parseFloat(prompt("Quanta distanza devi percorrere?"))
 
 const peopleAge = parseInt(prompt("Che età hai?"))
 
 document.getElementById('kmt').innerHTML = kilomNumbers + 'kmt'
-document.getElementById('età-pass').innerHTML = peopleAge + 'età-pass'
+document.getElementById('età-pass').innerHTML = peopleAge + ''
 
 let ticketPrice = kilomNumbers *0.21
 
@@ -11,10 +11,10 @@ let ticketMinAge = ticketPrice - (ticketPrice * 0.21)
 let ticketOverAge = ticketPrice - (ticketPrice * 0.39)
 
 if (peopleAge <= 18) {
-    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; ' + ticketMinAge.toFixed(2)
+    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; &euro; ' + ticketMinAge.toFixed(2)
 } else if (peopleAge >= 65){
-    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; ' + ticketOverAge.toFixed(2)
+    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; &euro; ' + ticketOverAge.toFixed(2)
 } else {
-    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; ' + ticketPrice.toFixed(2)
+    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; &euro;' + ticketPrice.toFixed(2)
 
 }
