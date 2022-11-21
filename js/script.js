60df0af610852c59c3c6bdcd345f446869efa740
FrancescoLoCasto <francesco_locasto@libero.it>
2022-11-21 16:32:20 +0100

    fix and start insert comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Question?
+
 const kilomNumbers = parseFloat(prompt("Quanta distanza devi percorrere?")); 
 
 const peopleAge = parseInt(prompt("Che età hai?"));
@@ -11,10 +13,10 @@ let ticketMinAge = ticketPrice - (ticketPrice * 0.21);
 let ticketOverAge = ticketPrice - (ticketPrice * 0.39);
 
 if (peopleAge <= 18) {
-    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; &euro; ' + ticketMinAge.toFixed(2)
+    document.getElementById('costo').innerHTML = 'Il costo del biglietto è &euro; ' + ticketMinAge.toFixed(2)
 } else if (peopleAge >= 65){
-    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; &euro; ' + ticketOverAge.toFixed(2)
+    document.getElementById('costo').innerHTML = 'Il costo del biglietto è &euro; ' + ticketOverAge.toFixed(2)
 } else {
-    document.getElementById('costo').innerHTML = 'Il costo del biglietto è; &euro;' + ticketPrice.toFixed(2)
+    document.getElementById('costo').innerHTML = 'Il costo del biglietto è s&euro;' + ticketPrice.toFixed(2)
 
 }
