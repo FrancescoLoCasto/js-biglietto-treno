729b415f5dbe45cf2f6fb720e4ee4925d12e4e7e
FrancescoLoCasto <francesco_locasto@libero.it>
2022-11-21 14:39:11 +0100

    continue insert content to script js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-const kilomNumbers = parseFloat(prompt(quanta distanza devi percorrere?))
+const kilomNumbers = parseFloat(prompt("quanta distanza devi percorrere?"))
 
-const peopleAge = parseInt(prompt(Che età hai?))
+const peopleAge = parseInt(prompt("Che età hai?"))
 
 document.getElementById('kmt').innerHTML = kilomNumbers + 'kmt'
 document.getElementById('età-pass').innerHTML = peopleAge + 'età-pass'
 
+let ticketPrice = kilomNumbers *0.21
+
+let ticketMinAge = ticketPrice - (ticketPrice * 0.21)
+let ticketOverAge = ticketPrice - (ticketPrice * 0.39)
+
+if (peopleAge <= 18) {
+    document.getElementById('costo')
+}
