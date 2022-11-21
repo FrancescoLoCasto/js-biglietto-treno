b04276b22d80f8043d525c2779384ec58d773bd9
FrancescoLoCasto <francesco_locasto@libero.it>
2022-11-21 14:29:36 +0100

    start insert content js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const kilomNumbers = parseFloat(prompt(quanta distanza devi percorrere?))
+
+const peopleAge = parseInt(prompt(Che età hai?))
+
+document.getElementById('kmt').innerHTML = kilomNumbers + 'kmt'
+document.getElementById('età-pass').innerHTML = peopleAge + 'età-pass'
+
