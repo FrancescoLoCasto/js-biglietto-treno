e68b3d501791728674e12b8a08514578cdffbc64
FrancescoLoCasto <francesco_locasto@libero.it>
2022-11-21 16:42:18 +0100

    insert all comments

--- js/script.js
+++ js/script.js
@@ -7,10 +7,15 @@ const peopleAge = parseInt(prompt("Che età hai?"));
 document.getElementById('kmt').innerHTML = kilomNumbers + 'km';
 document.getElementById('età-pass').innerHTML = peopleAge + '';
 
+//Ticket price
+
 let ticketPrice = kilomNumbers *0.21;
 
 let ticketMinAge = ticketPrice - (ticketPrice * 0.21);
 let ticketOverAge = ticketPrice - (ticketPrice * 0.39);
+
+//If and Else row
+
 
 if (peopleAge <= 18) {
     document.getElementById('costo').innerHTML = 'Il costo del biglietto è &euro; ' + ticketMinAge.toFixed(2)
